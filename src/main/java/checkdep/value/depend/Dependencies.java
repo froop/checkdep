8871737f3466bcd7a8f5f66f2049483a2dfe2a0f
froop <frooprogrammer@gmail.com>
2014-04-20 17:22:15 +0900

    refactoring

--- src/main/java/checkdep/value/depend/Dependencies.java
+++ src/main/java/checkdep/value/depend/Dependencies.java
@@ -1,14 +1,14 @@
 package checkdep.value.depend;
 
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
 public class Dependencies implements Iterable<Dependency> {
   private final ImmutableList<Dependency> list;
 
-  public Dependencies(List<Dependency> list) {
+  public Dependencies(Collection<Dependency> list) {
     this.list = new ImmutableList.Builder<Dependency>().addAll(list).build();
   }
 
