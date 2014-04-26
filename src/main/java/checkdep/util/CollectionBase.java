7caa307f1297a9eb9e8a536b8cd15f25d2a58f94
froop <frooprogrammer@gmail.com>
2014-04-26 18:42:10 +0900

    refactoring CollectionBase use ImmutableList.copyOf()

--- src/main/java/checkdep/util/CollectionBase.java
+++ src/main/java/checkdep/util/CollectionBase.java
@@ -10,7 +10,7 @@ public abstract class CollectionBase<E> extends ValueBase implements Iterable<E>
   private final ImmutableCollection<E> raw;
 
   protected CollectionBase(Collection<E> raw) {
-    this.raw = new ImmutableList.Builder<E>().addAll(raw).build();
+    this.raw = ImmutableList.copyOf(raw);
   }
 
   @Override
