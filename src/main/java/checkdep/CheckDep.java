4f49391cf80962caa92cb6d1fc74be132913f116
froop <frooprogrammer@gmail.com>
2014-04-27 17:46:31 +0900

    add CheckDep.check()

--- src/main/java/checkdep/CheckDep.java
+++ src/main/java/checkdep/CheckDep.java
@@ -1,10 +1,23 @@
 package checkdep;
 
 import checkdep.check.ConstraintChecker;
+import checkdep.check.Constraints;
+import checkdep.check.JDependConstraintChecker;
+import checkdep.parse.ExcludePackages;
 import checkdep.parse.ImportParser;
+import checkdep.parse.JDependImportParser;
+import checkdep.parse.SourceDirectories;
 import checkdep.value.violation.Violations;
 
 public class CheckDep {
+
+  public static Violations check(SourceDirectories directories,
+      ExcludePackages packages, Constraints constraints) {
+    return new CheckDep(
+        new JDependImportParser(directories, packages),
+        new JDependConstraintChecker(constraints)).execute();
+  }
+
   private final ImportParser parser;
   private final ConstraintChecker checker;
 
--- src/test/java/checkdep/CheckDepTest.java
+++ src/test/java/checkdep/CheckDepTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import checkdep.check.Constraints;
-import checkdep.check.JDependConstraintChecker;
 import checkdep.parse.ExcludePackages;
-import checkdep.parse.JDependImportParser;
 import checkdep.parse.SourceDirectories;
 import checkdep.value.violation.Violations;
 
@@ -15,35 +13,31 @@ public class CheckDepTest {
 
   @Test
   public void testExecute() {
-    CheckDep target = new CheckDep(
-        new JDependImportParser(
-            SourceDirectories.of(
-                "target/classes"),
-            ExcludePackages.of(
-                "java.lang",
-                "java.util",
-                "org.apache.commons.lang3",
-                "checkdep.util")),
-        new JDependConstraintChecker(
-            Constraints.builder()
-                .add("checkdep", "checkdep.check")
-                .add("checkdep", "checkdep.value.violation")
-                .add("checkdep", "checkdep.parse")
-                .add("checkdep.check", "checkdep.common")
-                .add("checkdep.check", "checkdep.value.depend")
-                .add("checkdep.check", "checkdep.value.violation")
-                .add("checkdep.check", "com.google.common.collect")
-                .add("checkdep.check", "jdepend.framework")
-                .add("checkdep.common", "checkdep.value.depend")
-                .add("checkdep.common", "jdepend.framework")
-                .add("checkdep.parse", "checkdep.common")
-                .add("checkdep.parse", "checkdep.value.depend")
-                .add("checkdep.parse", "java.io")
-                .add("checkdep.parse", "jdepend.framework")
-                .add("checkdep.value.violation", "checkdep.value.depend")
-                .build()));
-
-    Violations res = target.execute();
+    Violations res = CheckDep.check(
+        SourceDirectories.of(
+            "target/classes"),
+        ExcludePackages.of(
+            "java.lang",
+            "java.util",
+            "org.apache.commons.lang3",
+            "checkdep.util"),
+        Constraints.builder()
+            .add("checkdep", "checkdep.check")
+            .add("checkdep", "checkdep.value.violation")
+            .add("checkdep", "checkdep.parse")
+            .add("checkdep.check", "checkdep.common")
+            .add("checkdep.check", "checkdep.value.depend")
+            .add("checkdep.check", "checkdep.value.violation")
+            .add("checkdep.check", "com.google.common.collect")
+            .add("checkdep.check", "jdepend.framework")
+            .add("checkdep.common", "checkdep.value.depend")
+            .add("checkdep.common", "jdepend.framework")
+            .add("checkdep.parse", "checkdep.common")
+            .add("checkdep.parse", "checkdep.value.depend")
+            .add("checkdep.parse", "java.io")
+            .add("checkdep.parse", "jdepend.framework")
+            .add("checkdep.value.violation", "checkdep.value.depend")
+            .build());
 
     assertTrue(res.toString(), res.isEmpty());
   }
