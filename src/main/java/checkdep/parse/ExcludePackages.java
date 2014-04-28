5b96b8a0a7308b7c0d95002103cf948579a25aa7
froop <frooprogrammer@gmail.com>
2014-04-29 06:17:04 +0900

    extract ExcludePackages#toExcludePackageSet()

--- src/main/java/checkdep/parse/ExcludePackages.java
+++ src/main/java/checkdep/parse/ExcludePackages.java
@@ -2,6 +2,7 @@ package checkdep.parse;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import checkdep.util.CollectionBase;
@@ -12,10 +13,15 @@ public class ExcludePackages extends CollectionBase<ExcludePackage> {
     return ExcludePackages.of(Arrays.asList(directory));
   }
 
-  public static ExcludePackages of(Collection<String> packages) {
-    return new ExcludePackages(packages.stream()
+  public static ExcludePackages of(Collection<String> strings) {
+    return new ExcludePackages(toExcludePackageSet(strings));
+  }
+
+  private static Set<ExcludePackage> toExcludePackageSet(
+      Collection<String> strings) {
+    return strings.stream()
         .map(item -> new ExcludePackage(item))
-        .collect(Collectors.toSet()));
+        .collect(Collectors.toSet());
   }
 
   private ExcludePackages(Collection<ExcludePackage> directories) {
