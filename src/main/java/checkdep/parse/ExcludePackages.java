fed998c392adfb38b4536f0a0db3a3238d97df96
froop <frooprogrammer@gmail.com>
2014-04-29 07:48:10 +0900

    refactoring ExcludePackages inline

--- src/main/java/checkdep/parse/ExcludePackages.java
+++ src/main/java/checkdep/parse/ExcludePackages.java
@@ -2,7 +2,6 @@ package checkdep.parse;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import checkdep.util.CollectionBase;
@@ -14,14 +13,9 @@ public class ExcludePackages extends CollectionBase<ExcludePackage> {
   }
 
   public static ExcludePackages of(Collection<String> strings) {
-    return new ExcludePackages(toExcludePackageSet(strings));
-  }
-
-  private static Set<ExcludePackage> toExcludePackageSet(
-      Collection<String> strings) {
-    return strings.stream()
+    return new ExcludePackages(strings.stream()
         .map(item -> new ExcludePackage(item))
-        .collect(Collectors.toSet());
+        .collect(Collectors.toSet()));
   }
 
   private ExcludePackages(Collection<ExcludePackage> directories) {
