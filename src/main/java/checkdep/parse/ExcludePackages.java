5f11f2e299fc040ee53f2656e8b906479c093c6f
froop <frooprogrammer@gmail.com>
2014-04-28 23:37:43 +0900

    extract ExcludePackages to stream

--- src/main/java/checkdep/parse/ExcludePackages.java
+++ src/main/java/checkdep/parse/ExcludePackages.java
@@ -1,9 +1,8 @@
 package checkdep.parse;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
+import java.util.stream.Collectors;
 
 import checkdep.util.CollectionBase;
 
@@ -14,11 +13,9 @@ public class ExcludePackages extends CollectionBase<ExcludePackage> {
   }
 
   public static ExcludePackages of(Collection<String> packages) {
-    List<ExcludePackage> target = new ArrayList<>();
-    for (String item : packages) {
-      target.add(new ExcludePackage(item));
-    }
-    return new ExcludePackages(target);
+    return new ExcludePackages(packages.stream()
+        .map(item -> new ExcludePackage(item))
+        .collect(Collectors.toSet()));
   }
 
   private ExcludePackages(Collection<ExcludePackage> directories) {
