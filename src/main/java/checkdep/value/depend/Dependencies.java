ac24d8196db6f75de4b4186d8be3863a7190c555
froop <frooprogrammer@gmail.com>
2014-04-29 08:47:39 +0900

    refactoring Dependencies to stream

--- src/main/java/checkdep/value/depend/Dependencies.java
+++ src/main/java/checkdep/value/depend/Dependencies.java
@@ -1,17 +1,18 @@
 package checkdep.value.depend;
 
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import checkdep.util.MapBase;
 
 public class Dependencies extends MapBase<PackageName, Dependency> {
 
   public static Dependencies of(Collection<Dependency> collection) {
-    Map<PackageName, Dependency> map = new LinkedHashMap<>();
-    collection.forEach(item -> map.put(item.getName(), item));
-    return new Dependencies(map);
+    return new Dependencies(collection.stream()
+        .collect(Collectors.toMap(Dependency::getName,
+            Function.<Dependency>identity())));
   }
 
   private Dependencies(Map<PackageName, Dependency> map) {
