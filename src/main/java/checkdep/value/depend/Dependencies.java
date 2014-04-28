517b4b24a7b8f841cf002251e0ee8487adeea770
froop <frooprogrammer@gmail.com>
2014-04-29 08:48:50 +0900

    refactoring Dependencies to stream

--- src/main/java/checkdep/value/depend/Dependencies.java
+++ src/main/java/checkdep/value/depend/Dependencies.java
@@ -1,18 +1,17 @@
 package checkdep.value.depend;
 
+import static java.util.stream.Collectors.*;
+
 import java.util.Collection;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import checkdep.util.MapBase;
 
 public class Dependencies extends MapBase<PackageName, Dependency> {
 
   public static Dependencies of(Collection<Dependency> collection) {
-    return new Dependencies(collection.stream()
-        .collect(Collectors.toMap(Dependency::getName,
-            Function.<Dependency>identity())));
+    return new Dependencies(collection.stream().collect(
+        toMap(item -> item.getName(), item -> item)));
   }
 
   private Dependencies(Map<PackageName, Dependency> map) {
