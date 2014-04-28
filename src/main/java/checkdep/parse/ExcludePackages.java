ba8671732d2c13f1cc2324fd920f69b34ead473f
froop <frooprogrammer@gmail.com>
2014-04-29 07:45:18 +0900

    Revert "extract CollectionMapper"
    This reverts commit 2e731b598938820bc545d6f3596d5a19cdc7c70e.

--- src/main/java/checkdep/parse/ExcludePackages.java
+++ src/main/java/checkdep/parse/ExcludePackages.java
@@ -2,9 +2,10 @@ package checkdep.parse;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import checkdep.util.CollectionBase;
-import checkdep.util.CollectionMapper;
 
 public class ExcludePackages extends CollectionBase<ExcludePackage> {
 
@@ -13,13 +14,14 @@ public class ExcludePackages extends CollectionBase<ExcludePackage> {
   }
 
   public static ExcludePackages of(Collection<String> strings) {
-    return new ExcludePackages(toParticular(strings));
+    return new ExcludePackages(toExcludePackageSet(strings));
   }
 
-  private static Collection<ExcludePackage> toParticular(
+  private static Set<ExcludePackage> toExcludePackageSet(
       Collection<String> strings) {
-    return new CollectionMapper<String, ExcludePackage>(strings)
-        .toSet(raw -> new ExcludePackage(raw));
+    return strings.stream()
+        .map(item -> new ExcludePackage(item))
+        .collect(Collectors.toSet());
   }
 
   private ExcludePackages(Collection<ExcludePackage> directories) {
