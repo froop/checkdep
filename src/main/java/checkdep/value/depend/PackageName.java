5503c66fa28546970215ac0352808eb7568bd245
froop <frooprogrammer@gmail.com>
2014-04-29 17:17:08 +0900

    extract PackageName#matches()

--- src/main/java/checkdep/value/depend/PackageName.java
+++ src/main/java/checkdep/value/depend/PackageName.java
@@ -19,4 +19,21 @@ public class PackageName extends ValueBase implements Comparable<PackageName> {
   public int compareTo(PackageName other) {
     return getValue().compareTo(other.getValue());
   }
+
+  public boolean matches(PackageName other) {
+    String wildcard = ".*";
+    String str1 = this.getValue();
+    String str2 = other.getValue();
+    if (str1.endsWith(wildcard)) {
+      return matchesWithWildcard(str1, str2);
+    }
+    if (str2.endsWith(wildcard)) {
+      return matchesWithWildcard(str2, str1);
+    }
+    return str2.equals(str1);
+  }
+
+  private boolean matchesWithWildcard(String str1, String str2) {
+    return str2.startsWith(str1.replaceAll("\\.?\\*$", ""));
+  }
 }
--- src/main/java/checkdep/value/depend/PackageNames.java
+++ src/main/java/checkdep/value/depend/PackageNames.java
@@ -14,23 +14,6 @@ public class PackageNames extends CollectionBase<PackageName> {
   }
 
   public boolean contains(PackageName target) {
-    return stream().anyMatch(item -> matches(target, item));
-  }
-
-  private boolean matches(PackageName name1, PackageName name2) {
-    String wildcard = ".*";
-    String str1 = name1.getValue();
-    String str2 = name2.getValue();
-    if (str1.endsWith(wildcard)) {
-      return matchesWithWildcard(str1, str2);
-    }
-    if (str2.endsWith(wildcard)) {
-      return matchesWithWildcard(str2, str1);
-    }
-    return str2.equals(str1);
-  }
-
-  private boolean matchesWithWildcard(String str1, String str2) {
-    return str2.startsWith(str1.replaceAll("\\.?\\*$", ""));
+    return stream().anyMatch(target::matches);
   }
 }
