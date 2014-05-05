2d0d40e98a749b00deb2770029b751350ac54a9c
froop <frooprogrammer@gmail.com>
2014-05-05 17:43:14 +0900

    refactoring JDependDependency

--- src/main/java/checkdep/parse/JDependDependency.java
+++ src/main/java/checkdep/parse/JDependDependency.java
@@ -4,8 +4,8 @@ import static java.util.stream.Collectors.*;
 
 import java.util.Collection;
 
-import com.sun.istack.internal.NotNull;
 import jdepend.framework.JavaPackage;
+import lombok.NonNull;
 import checkdep.value.depend.Dependencies;
 import checkdep.value.depend.Dependency;
 import checkdep.value.depend.PackageName;
@@ -14,7 +14,7 @@ import checkdep.value.depend.PackageNames;
 class JDependDependency implements Dependency {
   private final JavaPackage raw;
 
-  private JDependDependency(@NotNull JavaPackage raw) {
+  private JDependDependency(@NonNull JavaPackage raw) {
     this.raw = raw;
   }
 
